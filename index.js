82623fa7470b5435c37c2bf5f2b1d2627dc1148b
Lucas de Brito <lucas.debrito@lexartlabs.com>
2021-11-30 05:10:21 -0300

    Init home page # feat: Created the function of added transactions and list they, the next step is make the page to confirm transactions and fix a little in the added modal

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Module = angular.module('indexPage').controller('indexController', ['$scope', '$state',
 function ($scope, $state) {
+    console.log($state)
     $state.go('login')
 }]);
--- src/app/components/balance/controllers/balanceCtrl.js
+++ src/app/components/balance/controllers/balanceCtrl.js
@@ -1,22 +1,41 @@
-const API_URL = "https://finances-webapp.herokuapp.com/"; // YOUR CRUD CRUD LINK HERE
+// const API_URL = "https://finances-webapp.herokuapp.com/"; // HEROKU API URL
+const API_URL = "http://localhost:4200/" // LOCALHOST API URL
 
 angular.module('indexPage').controller('balanceCtrl', ['$scope', '$state', '$http',
     function ($scope, $state, $http) {
         // States
-        $scope.session = JSON.parse(window.localStorage.getItem('userSession'));
+        $scope.session = JSON.parse(window.localStorage.getItem('userSession'))[0];
         $scope.typeMovement = '';
         $scope.idToEdit = '';
         $scope.itemInfos = {
             description: '',
             value: ''
         };
+
+        $scope.recipientDeposit = 0;
         $scope.itemValue = '';
-        $scope.inputsList = [
-            // {description: 'Teste1', value: 1000},{description: 'Teste1', value: 1000}
+        $scope.inputsList = [];
+        $scope.outputsList = [];
+
+        $scope.setRecipientDeposit = function (value) { 
+            $scope.recipientDeposit = value;
+            $scope.newMovement.accountIdRecipient = value == 0 ? $scope.session.accountId : null
+        }
+        
+        $scope.actionsList = [
+            { image: 'deposit', value: 1, text: 'Depósito' },
+            { image: 'transfer', value: 2, text: 'Transferência' },
+            { image: 'payment', value: 3, text: 'Solicitação de pagamento' },
+            { image: 'loan', value: 4, text: 'Empréstimo' }
         ];
-        $scope.outputsList = [
-            // {description: 'Teste1', value: 1000},{description: 'Teste1', value: 1000}
-        ];
+
+        $scope.newMovement = {
+            senderName: null,
+            accountIdSender: null,
+            accountIdRecipient: null,
+            typeTransaction: null,
+            amount: null
+        }
 
         // Methods
         $scope.startCtrl = function () {
@@ -27,86 +46,47 @@ angular.module('indexPage').controller('balanceCtrl', ['$scope', '$state', '$htt
             $scope.getTransactions(resIn => {
                 $scope.inputsList = resIn.data;
                 $scope.calculateBalances();
-                // $scope.getOutputs(resOut => {
-                //     $scope.outputsList = resOut.data;
-                // })
             })
         }
 
         $scope.getTransactions = function (callback) {
-            $http.get(API_URL + 'transactions/filter/account?accountId=1').then(res => {
+            $http.get(API_URL + `transactions/filter/account?accountId=${$scope.session.accountId}`).then(res => {
                 callback(res.data)
             })
         }
 
-        $scope.getOutputs = function (callback) {
-            $http.get(API_URL + '/outputs').then(res => {
-                callback({
-                    data: res.data
-                })
-            });
-        }
-
         $scope.calculateBalances = function () {
-            $scope.reducedInputs = $scope.inputsList.length > 0 ? $scope.inputsList.reduce((prev, next) => prev + next.amount, 0) : 0;
-            $scope.reducedOutputs = $scope.outputsList.length > 0 ? $scope.outputsList.reduce((prev, next) => prev + next.value, 0) : 0;
+            $scope.reducedInputs = $scope.inputsList.length > 0 ? $scope.inputsList.reduce((prev, next) => {
+                if(next.transactionStatus === 'completed') {
+                    if(next.accountIdSender == Number($scope.session.accountId)) {
+                        return prev - next.amount
+                    } else {
+                        return prev + next.amount
+                    }
+                } else {
+                    return prev
+                }
+            }, 0) : 0;
+
+            $scope.reducedPendingInputs = $scope.inputsList.length > 0 ? $scope.inputsList.reduce((prev, next) => {
+                if(next.transactionStatus === 'pending') {
+                    return prev + next.amount
+                } else {
+                    return prev
+                }
+            }, 0) : 0;
         }
 
-        $scope.setTypeMovement = function (type, item) {
-            $scope.typeMovement = type
-            if (item) {
-                $scope.itemInfos.description = item.description;
-                $scope.itemInfos.value = item.value;
-                $scope.idToEdit = item._id;
-            } else {
-                $scope.itemInfos.description = "";
-                $scope.itemInfos.value = "";
-                $scope.idToEdit = "";
-            }
-        }
-
-        $scope.addNewItem = function (newItem) {
-            $scope.typeMovement === 'Entrada'
-                ? $scope.addNewInput(newItem)
-                : $scope.addNewOutput(newItem);
-        }
-
-        $scope.editItem = function (editingItem) {
-            const isInput = $scope.inputsList.filter(ele => ele._id === $scope.idToEdit)[0];
-            $http.put(API_URL + (isInput ? '/inputs/' : '/outputs/') + $scope.idToEdit, editingItem).then(() => {
-                isInput
-                    ? $scope.getInputs(res => {
-                        $scope.inputsList = res.data;
-                        $scope.calculateBalances();
-                    })
-                    : $scope.getOutputs(res => {
-                        $scope.outputsList = res.data;
-                        $scope.calculateBalances();
-                    })
-            })
-        }
-
-        $scope.addNewInput = function (newInput) {
-            const teste = {
-                accountIdSender: 1,
-                accountIdRecipient: 1, 
-                typeTransaction: 1,
-                amount: 100
-            }
-            $http.post(API_URL + 'transactions/new', teste).then(() => {
-                $scope.getInputs(res => {
-                    $scope.inputsList = res.data;
-                    $scope.calculateBalances();
-                })
-            })
-        }
-
-        $scope.addNewOutput = function (newOutput) {
-            $http.post(API_URL + '/outputs', newOutput).then(() => {
-                $scope.getOutputs(res => {
-                    $scope.outputsList = res.data;
-                    $scope.calculateBalances();
-                })
+        $scope.addNewTransaction = function () {
+            $scope.newMovement.senderName = $scope.session.name;
+            $http.post(API_URL + 'transactions/new', $scope.newMovement).then(() => {
+                $scope.startCtrl();
+                $scope.newMovement = {
+                    accountIdSender: null,
+                    accountIdRecipient: null,
+                    typeTransaction: null,
+                    amount: null
+                }
             })
         }
 
