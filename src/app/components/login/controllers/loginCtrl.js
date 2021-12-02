b9ac47034d428e2dbad1e5d61098923b9730f158
Lucas de Brito <lucas.debrito@lexartlabs.com>
2021-12-02 10:26:13 -0300

    API_URL - height

--- src/app/components/login/controllers/loginCtrl.js
+++ src/app/components/login/controllers/loginCtrl.js
@@ -1,25 +1,25 @@
 
-const API_URL = "https://finances-webapp.herokuapp.com/"; // HEROKU API URL
+// const API_URL = "https://finances-webapp.herokuapp.com/"; // HEROKU API URL
 // const API = "http://localhost:4200/" // LOCALHOST API URL
 var Module = angular.module('indexPage').controller('loginCtrl', ['$scope', '$http', '$state',
 
-function ($scope, $http, $state) {
+    function ($scope, $http, $state) {
         $scope.email = "";
         $scope.password = "";
         $scope.loginMessage = "";
 
-        $scope.startComponent = function() {
-            if(window.localStorage.getItem('userSession')) {
+        $scope.startComponent = function () {
+            if (window.localStorage.getItem('userSession')) {
                 $state.go('balance')
             }
         }
 
         $scope.login = function () {
             $scope.loginMessage = "";
-            $http.post(API + 'accounts/login', {email: $scope.email, password: $scope.password}).then((res) => {
+            $http.post(API_URL + 'accounts/login', { email: $scope.email, password: $scope.password }).then((res) => {
                 let session = res.data.data;
                 window.localStorage.setItem('userSession', JSON.stringify(session))
-                if(session[0].name.toLowerCase() === 'administrador') $state.go('admin-dashboard')
+                if (session[0].name.toLowerCase() === 'administrador') $state.go('admin-dashboard')
                 else $state.go('balance')
             }).catch(err => {
                 $scope.loginMessage = err.data.message;
